b0ab2921f9542a34cebb0db06f62db2d6f4417d5
Tom <111147401+1OSaft@users.noreply.github.com>
2024-02-06 08:19:12 +0100

    Add files via upload

--- client/client.lua
+++ client/client.lua
@@ -1,23 +1,27 @@
 local bagEquipped, skin
 local ox_inventory = exports.ox_inventory
 local ped = cache.ped
-local justConnect = true
 local count = 0
 local timeout, changed, puttedon = false, false, false
+local PlayerState = LocalPlayer.state
 
 local function PutOnBag(bagtype)
     bagtype = bagtype
     if Config.Debug then print("Putting on Backpack") end
     if Config.Debug then print("Bag type: "..bagtype) end
-    TriggerEvent('skinchanger:getSkin', function(skin)
-        if skin.sex == 0 then
-            TriggerEvent('skinchanger:loadClothes', skin, Config.Backpacks[bagtype].Uniform.Male)
-        else
-            TriggerEvent('skinchanger:loadClothes', skin, Config.Backpacks[bagtype].Uniform.Female)
-        end
-        saveSkin()
-    end)
-    bagEquipped = true
+    if Config.Framework == 'ESX' then
+        TriggerEvent('skinchanger:getSkin', function(skin)
+            if skin.sex == 0 then
+                TriggerEvent('skinchanger:loadClothes', skin, Config.Backpacks[bagtype].Uniform.Male)
+            else
+                TriggerEvent('skinchanger:loadClothes', skin, Config.Backpacks[bagtype].Uniform.Female)
+            end
+            saveSkin()
+        end)
+    elseif Config.Framework == 'ND' then
+        local appearance = fivemAppearance:getPedAppearance(cache.ped)
+    end
+    PlayerState:set('backpack', true)
 end
 
 saveSkin = function()
@@ -42,87 +46,97 @@ local function RemoveBag(bagtype)
     end)
 end
 --]]
+
+AddStateBagChangeHandler('backpack', nil, function(bagName, _, backItems, _, replicated)
+    if replicated then
+        return
+    end
+end)
+
 local function RemoveBag(bagtype)
     bagtype = bagtype
     if Config.Debug then print("Removing Backpack") end
     TriggerEvent('skinchanger:getSkin', function(skin)
-        print(skin)
-        print(bagtype)
         local clothesWithoutBag
-        if skin.sex == 0 then
+        if skin.sex == 0 then56
             clothesWithoutBag = Config.CleanUniform.Male
         else
             clothesWithoutBag = Config.CleanUniform.Female
         end
         TriggerEvent('skinchanger:loadClothes', skin, clothesWithoutBag)
         saveSkin()
-        bagEquipped = nil
+        PlayerState:set('backpack', false)
     end)
 end
 
-AddEventHandler('ox_inventory:updateInventory', function(changes)
-    if justConnect then
-        Wait(4500)
-        justConnect = nil
-    end
-    if Config.Debug then print("Update Inv") end
-    for k, v in pairs(changes) do
-        if not changed then
-            changed = true
-            if Config.Debug then print("V: "..tostring(v)) end
-            if type(v) == 'table' then
-                if not timeout then
-                    timeout = true
-                    local count = 0
-                    for vbag in pairs(Config.Backpacks) do
-                            count = ox_inventory:Search('count', vbag)
-                            if count > 0 and (not bagEquipped) then
-                                if count >= 1 then
-                                    PutOnBag(vbag)
-                                    if Config.Debug then print("Count: "..count) end
-                                end
-                            end
-                    end
-                end
-                timeout = false
-            elseif type(v) == 'boolean' then
-                if not timeout then
-                    local count = 0
-                    for vbag in pairs(Config.Backpacks) do
-                        count = count + ox_inventory:Search('count', vbag)
-                    end
-                    if count > 0 and (not bagEquipped) then
-                        if count >= 1 then
-                            PutOnBag(vbag)
-                            if Config.Debug then print("Count: "..count) end
-                        end                                
-                    else
-                        if count == 0 and bagEquipped then
-                            RemoveBag(vbag)
-                        end
-                    end
+function tableChange(data)
+    local count = 0
+
+    for vbag in pairs(Config.Backpacks) do
+        count = ox_inventory:Search('count', vbag)
+        local bag = PlayerState.backpack
+        if count > 0 and bag == false then
+            if count >= 1 then
+                PutOnBag(vbag)
+                if Config.Debug then
+                    print("Count: " .. count)
                 end
             end
         end
-
     end
-    changed = false
+end
+
+function boolChange()
+    local count = 0
+
+    for vbag in pairs(Config.Backpacks) do
+        count = count + ox_inventory:Search('count', vbag)
+    end
+    local bag = PlayerState.backpack
+    if count > 0 and bag == false then
+        if count >= 1 then
+            PutOnBag(vbag)
+            if Config.Debug then
+                print("Count: " .. count)
+            end
+        end
+    elseif count == 0 and bagEquipped then
+        RemoveBag(vbag)
+    end
+end
+
+AddEventHandler('ox_inventory:updateInventory', function(changed)
+    if changed then return end
+    for k, v in pairs(changed) do
+        if type(v) == 'table' and not timeout then
+            timeout = true
+            tableChange(v)
+            timeout = false
+        elseif type(v) == 'boolean' and not timeout then
+            boolChange()
+        end
+    end
 end)
+
 lib.onCache('ped', function(value)
     ped = value
 end)
 
+function CountBackpacksInInventory()
+    local count = 0
+    for vbag in pairs(Config.Backpacks) do
+        count = count + ox_inventory:Search('count', vbag)
+    end
+    return count
+end
+
 lib.onCache('vehicle', function(value)
     if GetResourceState('ox_inventory') ~= 'started' then return end
+    local count = CountBackpacksInInventory()
     if value then
         RemoveBag()
-    else
-        for vbag in pairs(Config.Backpacks) do
-			count = count + ox_inventory:Search('count', vbag)
-		end
-        if count and count >= 1 then
-            PutOnBag(bagtype)
-        end
+    elseif count >= 1 then
+        PutOnBag(bagtype)
     end
 end)
 
